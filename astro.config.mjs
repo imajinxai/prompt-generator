5192f009e8906f043c9490771120a8edab5ea99e
imajinxai <fastlanemvp@gmail.com>
2026-01-18 07:17:21 +0700

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://promptcraft.imajinx.co',
+  site: 'https://google-prompt-generator.vercel.app',
   integrations: [
     react(),
     tailwind(),
